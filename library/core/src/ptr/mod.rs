a6396c3d64b3846306137bd762f5e277d41331e2
Endre Bakka <endre.bakka@sonair.com>
2025-12-04 18:48:38 +0000

    ptr::addr_of_mut

--- library/core/src/ptr/mod.rs
+++ library/core/src/ptr/mod.rs
@@ -2769,7 +2769,6 @@ pub macro addr_of($place:expr) {
 /// makes no difference whether the pointer is null or dangling.)
 #[stable(feature = "raw_ref_macros", since = "1.51.0")]
 #[rustc_macro_transparency = "semitransparent"]
-#[cfg(not(feature = "ferrocene_certified"))]
 pub macro addr_of_mut($place:expr) {
     &raw mut $place
 }
